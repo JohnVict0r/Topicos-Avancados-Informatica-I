cc23307beaabf6e013e00525137e3b116d695669
John Victor <johnvictorio3@gmail.com>
2018-08-22 23:23:40 -0300

    criado função sinal para ativação do perceptron

--- Perceptron.py
+++ Perceptron.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-Este é um arquivo de script temporário.
+@AUTOR 
+    John Victor - ECT - UFRN - 2018
 """
+
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -16,35 +17,64 @@ class perceptron(object):
         self.w = w
         self.bias = bias
         self.limiar = limiar
+        
+        
+    def ativacao_sinal(self, u):
+        if u>= 0:
+            return 1
+        else:
+            return -1
+        
     def atualizar_pesos(self, xi, yi, u):
+        
+        print("atualizando...")       
         for i in range(len(self.w)):
-            self.w[i] += self.taxa_aprendizado*xi[i]*(yi - u)
-            
+            self.w[i] += self.taxa_aprendizado*xi[i]*(yi-u)
         self.limiar += self.taxa_aprendizado*(-1)*(yi - u)
+        
+        print("vetor de pesos atualizado = %s \nlimiar = %s" % (self.w, self.limiar))
+        
     def treinar_rede(self, x_treino, y_treino):
         for xi,yi in zip(x_treino,y_treino):
-            m = np.multiply(xi,self.w) 
+          
+            #multiplicar o vetor de caracteristicas(X) pelo vetor de pesos(W)
+            m = np.inner(xi,self.w)            
             u = np.sum(m) - self.limiar
-            if u != yi:
-                self.atualizar_pesos(xi,yi,u)
+            
+            #encontrar o valor gerado pela função de ativação
+            y=self.ativacao_sinal(u)
+            
+            #verificar se o valor desejado é igual ao valor gerado pela rede
+            if (y-yi) != 0:
+                
+                #senao for igual, a rede deve atualizar os pesos para tentar se adaptar ao formato dos dados, ou padrão...
+                self.atualizar_pesos(xi,yi,y)
+        
     def previsao(self, x_teste):
         m = np.multiply(x_teste,self.w)
         u = np.sum(m) - self.limiar
-        if u< 0:
-            return -1
-        else:
-            return 1
+        
+        return self.ativacao_sinal(u)
+
 
 if __name__ == '__main__':    
     
     x_treino = [[0,0,1], [1,1,0]]
     y_treino = [-1,1]
     
-    x_teste=[0,1,1]
+    x1=[1,1,1]
+    x2=[0,0,0]
+    x3=[1,0,0]
+    x4=[0,1,1]
     
-    # usa perceptron para separar os dados	
+            
     clf = perceptron(taxa_aprendizado = 0.4,limiar=0.5)
     clf.treinar_rede(x_treino,y_treino)
-    y_prev = clf.previsao(x_teste)
     
-    print(y_prev)
+    y1 = clf.previsao(x1)
+    y2 = clf.previsao(x2)
+    y3 = clf.previsao(x3)
+    y4 = clf.previsao(x4)
+    
+    print(y1,y2,y3,y4)
+    
