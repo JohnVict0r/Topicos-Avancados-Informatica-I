b6b50bb6e5abd57582e3de133d01cd366c57f80e
John Victor <30505330+JohnVictor2017@users.noreply.github.com>
2018-11-05 17:16:47 -0300

    Update LinearRegression_basic.py

--- LinearRegression_basic.py
+++ LinearRegression_basic.py
@@ -14,3 +14,14 @@ y = [m*r + b for r in months]
 plt.plot(months, y)
 
 plt.show()
+
+def get_gradient_at_b(x, y, m, b): 
+  diff = 0 
+  N = len(x) 
+  for i in range(0, len(x)): 
+    y_val = y[i] 
+    x_val = x[i]
+    diff += (y_val - ((m * x_val) + b)) 
+    
+  b_gradient = -2/N * diff
+  return b_gradient
