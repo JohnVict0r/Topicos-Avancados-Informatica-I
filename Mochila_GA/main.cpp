7d59a1932467b66b0d8ff511de5c98c0f13a7775
John Victor <30505330+JohnVictor2017@users.noreply.github.com>
2018-11-08 16:10:30 -0300

    Ler arquivos: config e lista_itens

--- Mochila_GA/main.cpp
+++ Mochila_GA/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-#define MAX_GENES 50
+#define MAX_GENES 100
 #define MAX_ITEMS 20
 #define MAX_NAME 100
 #define MAX_CROMOSSOMOS 100
@@ -14,7 +14,17 @@ using namespace std;
 #include <sstream>
 #include <cstdlib>
 
+
+
+
 struct Item{
+
+    string nome;
+    string beneficio;
+    string peso;
+
+};
+struct ItemInt{
 
     string nome;
     int beneficio;
@@ -61,38 +71,34 @@ struct ConfigInt
 
 };
 
+void imprimir_itens(ItemInt i[MAX_ITEMS], unsigned tam);
 
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
 
-    ConfigInt config;
-    Item itens[MAX_ITEMS];
+    ConfigInt config[10];
+    ItemInt itens[MAX_ITEMS];
 
-    int qt_config=0, qt_itens=0;
+    int qt_itens=0;
 
-    ifstream ip("\config.csv");
+    ifstream ip("config.txt");
     if(ip.is_open())
     {
         while(ip.good())
         {
-            if(qt_config==0)
-            {
-                qt_config++;
-                continue;
-            }
-
             ConfigString t;
 
-            getline(ip,t.peso_suportado,';');
-            getline(ip,t.populacao,';');
+            getline(ip,t.peso_suportado,',');
+            getline(ip,t.populacao,',');
+            getline(ip,t.qt_itens,',');
             getline(ip,t.timeout_sec,'\n');
 
-
-            config.peso_suportado=atoi(t.peso_suportado.c_str());
-            config.populacao=atoi(t.populacao.c_str());
-            config.qt_itens=atoi(t.qt_itens.c_str());
-            config.timeout_sec=atoi(t.timeout_sec.c_str());
+            config[qt_itens].peso_suportado=atoi(t.peso_suportado.c_str());
+            config[qt_itens].populacao=atoi(t.populacao.c_str());
+            config[qt_itens].qt_itens=atoi(t.qt_itens.c_str());
+            config[qt_itens].timeout_sec=atoi(t.timeout_sec.c_str());
+            qt_itens++;
         }
         ip.close();
     }
@@ -101,42 +107,46 @@ int main()
         cout<<"ERRO AO CARREGAR AS CONFIGURAÇÕES!";
     }
 
+    qt_itens=0;
 
-    ifstream ip("\items.csv");
-    if(ip.is_open())
+    ifstream it("lista_itens.txt");
+    if(it.is_open())
     {
-        while(ip.good())
+        while(it.good())
         {
-            if(qt_itens==0)
-            {
-                qt_itens++;
-                continue;
-            }
-
             Item i;
 
+            getline(it,i.nome,',');
+            getline(it,i.beneficio,',');
+            getline(it,i.peso,'\n');
 
-            getline(ip,i.nome,';');
-            getline(ip,i.beneficio,';');
-            getline(ip,i.peso,'\n');
-
-            itens[qt_itens-1].nome=i.nome;
-            itens[qt_itens-1].beneficio=atoi(i.beneficio.c_str());
-            itens[qt_itens-1].peso=atoi(i.peso.c_str());
+            itens[qt_itens].nome=i.nome;
+            itens[qt_itens].beneficio=atoi(i.beneficio.c_str());
+            itens[qt_itens].peso=atoi(i.peso.c_str());
 
             qt_itens++;
-            }
-
         }
 
-        inscrito[qt].id='\0';
-        ip.close();
+        it.close();
 
     }
     else
     {
-        cout<<"ERRO!";
+        cout<<"ERRO AO LER OS ITENS!";
     }
 
+    imprimir_itens(itens,config[0].qt_itens);
+
     return 0;
+}
+
+
+void imprimir_itens(ItemInt item[MAX_ITEMS], unsigned tam){
+
+for(unsigned i=0; i<tam; i++){
+
+    cout<<item[i].nome<<" / "<<item[i].beneficio<<" / "<<item[i].peso<<endl;
+
+}
+
 }
